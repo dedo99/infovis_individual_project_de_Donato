903a964087de5906a767235411f9b49b1dc257cb
dedo99 <andreadedonato99@gmail.com>
2023-05-07 19:00:39 +0200

    Start project

---
+++ hot_air_balloon/script.js
@@ -0,0 +1,24 @@
+
+// pos_x -> la posizione orizzontale della mongolfiera
+// pos_y -> la posizione verticale della mongolfiera
+// balloon_size -> la grandezza del pallone della mongolfiera
+// basket_size -> la grandezza del cesto della mongolfiera
+// color -> il colore della mongolfiera
+
+var dataSet = [
+    {"pos_x": 50, "pos_y": 150, "balloon_size": 80, "basket_size": 60, "color": 255},
+    {"pos_x": 200, "pos_y": 50, "balloon_size": 100, "basket_size": 80, "color": 0},
+    {"pos_x": 100, "pos_y": 100, "balloon_size": 60, "basket_size": 50, "color": 150},
+    {"pos_x": 250, "pos_y": 200, "balloon_size": 120, "basket_size": 70, "color": 50},
+    {"pos_x": 150, "pos_y": 250, "balloon_size": 70, "basket_size": 50, "color": 200},
+    {"pos_x": 75, "pos_y": 50, "balloon_size": 90, "basket_size": 60, "color": 100},
+    {"pos_x": 225, "pos_y": 150, "balloon_size": 80, "basket_size": 70, "color": 180},
+    {"pos_x": 50, "pos_y": 200, "balloon_size": 110, "basket_size": 80, "color": 20},
+    {"pos_x": 175, "pos_y": 100, "balloon_size": 60, "basket_size": 50, "color": 230},
+    {"pos_x": 300, "pos_y": 150, "balloon_size": 100, "basket_size": 70, "color": 120}
+]
+
+
+
+
+
